b53237df17fef02b9ed1e649505a6f518d5a48cf
Davi Freitas <davifreitasdebrito909@gmail.com>
2025-04-15 14:29:13 -0300

    Add files via upload

---
+++ davi freitas/JavaScript/Aula-08/atividade2.js
@@ -0,0 +1,5 @@
+for (impares = 0; impares <= 50; impares++) {
+    if(impares % 2 !== 0) {
+        console.log(impares)
+    }
+}
---
+++ davi freitas/JavaScript/Aula-08/repetição.js
@@ -0,0 +1,6 @@
+let tiposDeBanana = ['da terra', 'nanica', 'prata', 'maça', 'ouro', 'pão']
+for (contador = 0; contador < tiposDeBanana.length; contador++) {
+    tiposDeBanana[contador] = tiposDeBanana[contador]
+    console.log(`${contador + 1} ${tiposDeBanana[contador]}`);
+
+}
