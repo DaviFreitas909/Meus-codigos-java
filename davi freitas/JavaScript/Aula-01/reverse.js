1013c41e8974aa1fe40b5d3c7023e1d55487686a
Davi Freitas <davifreitasdebrito909@gmail.com>
2025-05-05 17:04:05 -0300

    Delete davi freitas directory

--- davi freitas/JavaScript/Aula-01/reverse.js
+++
@@ -1,3 +0,0 @@
-let numeros = [10,20,30,40,50,60,70]
-numeros.reverse()
-console.log(numeros)
--- davi freitas/JavaScript/Aula-01/start.js
+++
@@ -1,6 +0,0 @@
-let nome = "Eduardo"
-let idade = 18
-let praia = true
-nome = "Ronaldo"
-praia = false
-console.log("Bem vindo", nome,"voce tem", idade,"anos, e será que gosta de praia?", praia)
