a62c75c32c87eabbd3a95a17a31c1f00af6594dd
Davi Freitas <davifreitasdebrito909@gmail.com>
2025-05-05 17:05:00 -0300

    Add files via upload

---
+++ davi freitas/JavaScript/Aula-05/metodosString.js
@@ -0,0 +1,8 @@
+let nome = 'davi freitas'
+console.log(nome[0]); //mostra o peimwieo indice da string
+console.log(nome.length) //conta os caracteres no terminal
+console.log(nome.charAt(1)); // mostra o o caractere dependendo do indice adicionado
+console.log(nome.toLowerCase()); //mostra toda a variavel no terminal
+console.log(nome.toUpperCase()); //transforma todas as letras em letras maiusculas
+console.log(nome.split());
+console.log(nome.substring(0, 6)); // corta a sring a partir do indice adionado ao parenteses
---
+++ davi freitas/JavaScript/Aula-10/subitração.js
@@ -0,0 +1,13 @@
+function sub(a, b) {
+    console.log(b - a)
+}
+function soma(a, b) {
+    console.log(a + b)
+}
+function divi(a, b) {
+    console.log(a / b)
+}
+function mult(a, b) {
+    console.log(a * b)
+} 
+soma(1, 2)
