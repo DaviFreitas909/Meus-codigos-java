89f89dc41987b3312949c5802a05a89bbb0b8229
Davi Freitas <davifreitasdebrito909@gmail.com>
2025-05-13 15:33:58 -0300

    Add files via upload

---
+++ davi freitas/JS/Aula-6/exemplo1.js
@@ -0,0 +1,9 @@
+let idade = 18
+
+if (idade => 18) {
+    console.log('Opa mano vejo que voce e maior de idade pode adrentrar');
+}
+
+else {
+    console.log('Voce esta barrado por ser menor da idade');
+}
